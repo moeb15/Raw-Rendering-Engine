d980ac8cbfa6d3866e515448f60ecb3c13bdf6cb
Mujtaba Bader <mujtaba.bader@hotmail.com>
2025-07-03 04:41:24 -0400

    texture loader

---
+++ raw_renderer/include/resources/texture_loader.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "renderer/gpu_resources.hpp"
+#include "resources/resource_manager.hpp"
+#include <memory>
+#include <unordered_map>
+
+namespace Raw
+{
+#define DEFAULT_TEXTURE "default"
+#define DEFAULT_EMISSIVE "defaultEmissive"
+#define ERROR_TEXTURE "error"
+
+    struct TextureResource : public Resource
+    {
+        GFX::TextureHandle handle;
+        u64 textureId;
+
+        static constexpr cstring k_ResourceType = "texture_resource";
+    };
+
+    class TextureLoader : public IResourceLoader
+    {
+    public:
+        static TextureLoader* Instance();
+
+        virtual void Init() override;
+        virtual void Shutdown() override;
+        virtual Resource* Get(cstring name) override;
+        virtual Resource* Get(u64 hashedName) override;
+        virtual void Unload(cstring name) override;
+        virtual Resource* CreateFromFile(cstring name, cstring filename) override;
+        Resource* CreateFromData(cstring name, const GFX::TextureDesc& desc, void* data);
+
+    private:
+        std::unordered_map<u64, std::unique_ptr<TextureResource>> m_TextureMap;
+
+    };
+}
---
+++ raw_renderer/src/resources/texture_loader.cpp
@@ -0,0 +1,129 @@
+#include "resources/texture_loader.hpp"
+#include "renderer/gfxdevice.hpp"
+#include "core/servicelocator.hpp"
+#include "core/logger.hpp"
+#include "utility/hash.hpp"
+#include "stb_image.h"
+
+namespace Raw
+{
+    static TextureLoader s_TextureLoader;
+
+    TextureLoader* TextureLoader::Instance()
+    {
+        return &s_TextureLoader;
+    }
+
+    void TextureLoader::Init()
+    {
+        TextureLoader::Instance()->CreateFromFile(ERROR_TEXTURE, "C:/Users/Mujta/VSCode Projects/raw/assets/Error.png");
+        TextureLoader::Instance()->CreateFromFile(DEFAULT_TEXTURE, "C:/Users/Mujta/VSCode Projects/raw/assets/Default.png");
+        TextureLoader::Instance()->CreateFromFile(DEFAULT_EMISSIVE, "C:/Users/Mujta/VSCode Projects/raw/assets/DefaultEmissive.png");
+    }   
+
+    void TextureLoader::Shutdown()
+    {
+        RAW_INFO("TextureLoader shutting down...");
+        for(auto& kvPair : m_TextureMap)
+        {
+            kvPair.second.reset();
+        }
+        m_TextureMap.clear();
+    }
+
+    Resource* TextureLoader::Get(cstring name)
+    {
+        u64 hashedName = Utils::HashCString(name);
+        if(m_TextureMap.find(hashedName) != m_TextureMap.end())
+        {
+            m_TextureMap[hashedName]->AddRef();
+            return m_TextureMap[hashedName].get();
+        }
+        
+        RAW_DEBUG("TextureLoader could not find texture '%s'", name);
+        u64 errHash = Utils::HashCString(ERROR_TEXTURE);
+        return m_TextureMap[errHash].get();
+    }
+
+    Resource* TextureLoader::Get(u64 hashedName)
+    {
+        if(m_TextureMap.find(hashedName) != m_TextureMap.end())
+        {
+            m_TextureMap[hashedName]->AddRef();
+            return m_TextureMap[hashedName].get();
+        }
+        
+        RAW_DEBUG("TextureLoader could not find texture id: %llu", hashedName);
+        u64 errHash = Utils::HashCString(ERROR_TEXTURE);
+        return m_TextureMap[errHash].get();
+    }
+
+    void TextureLoader::Unload(cstring name)
+    {
+        u64 hashedName = Utils::HashCString(name);
+        if(m_TextureMap.find(hashedName) != m_TextureMap.end())
+        {
+            m_TextureMap[hashedName]->RemoveRef();
+            if(m_TextureMap[hashedName]->refs == 0)
+            {
+                GFX::IGFXDevice* device = (GFX::IGFXDevice*)ServiceLocator::Get()->GetService(GFX::IGFXDevice::k_ServiceName);
+                TextureResource* res = m_TextureMap[hashedName].get();
+                device->DestroyTexture(res->handle);
+
+                m_TextureMap.erase(hashedName);
+            }
+        }
+        RAW_DEBUG("TextureLoader could not unload texture '%s'", name);
+    }
+
+    Resource* TextureLoader::CreateFromFile(cstring name, cstring filename)
+    {
+        u64 hashedName = Utils::HashCString(name);
+        if(m_TextureMap.find(hashedName) != m_TextureMap.end()) return nullptr;
+        
+        int w, h, numCh;
+        u8* imageData = stbi_load(filename, &w, &h, &numCh, 4);
+        if(!imageData)
+        {
+            RAW_ERROR("TextureLoader failed to load image '%s', file path '%s'", name, filename);
+            return nullptr;
+        }
+        GFX::TextureDesc desc;
+        desc.width = (u32)w;
+        desc.height = (u32)h;
+        desc.depth = 1;
+        desc.format = GFX::ETextureFormat::R8G8B8A8_UNORM;
+        desc.isMipmapped = true;
+        desc.isRenderTarget = false;
+        desc.isStorageImage = false;
+        desc.type = GFX::ETextureType::TEXTURE2D;
+        std::unique_ptr<TextureResource> tex = std::make_unique<TextureResource>();
+        tex->name = name;
+        tex->textureId = hashedName;
+
+        GFX::IGFXDevice* device = (GFX::IGFXDevice*)ServiceLocator::Get()->GetService(GFX::IGFXDevice::k_ServiceName);
+        tex->handle = device->CreateTexture(desc, imageData);
+        tex->AddRef();
+
+        m_TextureMap.insert(std::pair<u64, std::unique_ptr<TextureResource>>(hashedName, std::move(tex)));
+
+        return tex.get();
+    }
+
+    Resource* TextureLoader::CreateFromData(cstring name, const GFX::TextureDesc& desc, void* data)
+    {
+        u64 hashedName = Utils::HashCString(name);
+        if(m_TextureMap.find(hashedName) != m_TextureMap.end()) return nullptr;
+
+        std::unique_ptr<TextureResource> tex = std::make_unique<TextureResource>();
+        tex->name = name;
+        tex->textureId = hashedName;
+
+        GFX::IGFXDevice* device = (GFX::IGFXDevice*)ServiceLocator::Get()->GetService(GFX::IGFXDevice::k_ServiceName);
+        tex->handle = device->CreateTexture(desc, data);
+        tex->AddRef();
+
+        m_TextureMap.insert(std::pair<u64, std::unique_ptr<TextureResource>>(hashedName, std::move(tex)));
+        return tex.get();
+    }
+}
