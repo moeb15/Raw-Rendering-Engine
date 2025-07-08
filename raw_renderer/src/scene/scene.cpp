b455fa4b17cf6c78fb4fbafb75cbe3181ff26053
Mujtaba Bader <mujtaba.bader@hotmail.com>
2025-07-08 10:48:08 -0400

    unloading previous scene if scene data is not null

--- raw_renderer/src/scene/scene.cpp
+++ raw_renderer/src/scene/scene.cpp
@@ -1,11 +1,25 @@
 #include "scene/scene.hpp"
 #include "utility/gltf.hpp"
+#include "resources/buffer_loader.hpp"
+#include "resources/texture_loader.hpp"
 
 namespace Raw
 {
     void Scene::Init(std::string& filePath)
     {
-        if(m_SceneData) m_SceneData.reset();
+        if(m_SceneData)
+        {
+            BufferLoader::Instance()->Unload(m_SceneData->vertexBufferId);
+            BufferLoader::Instance()->Unload(m_SceneData->indexBufferId);
+            BufferLoader::Instance()->Unload(m_SceneData->indirectBufferId);
+
+            for(u64 i = 0; i < m_SceneData->imageIds.size(); i++)
+            {
+                TextureLoader::Instance()->Unload(m_SceneData->imageIds[i]);
+            }
+
+            m_SceneData.reset();
+        }
 
         m_SceneData = std::make_unique<GFX::SceneData>();
         Utils::LoadGLTF(filePath, *m_SceneData.get());
