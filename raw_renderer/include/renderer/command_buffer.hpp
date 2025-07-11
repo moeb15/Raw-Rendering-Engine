02eee418c44af7602716fd143de6fec6b9e2f091
Mujtaba Bader <mujtaba.bader@hotmail.com>
2025-07-11 11:14:29 -0400

    added additional add memory barrier method for buffers

--- raw_renderer/include/renderer/command_buffer.hpp
+++ raw_renderer/include/renderer/command_buffer.hpp
@@ -22,13 +22,14 @@ namespace Raw::GFX
         virtual void Dispatch(const ComputePipelineHandle& handle, u32 groupX, u32 groupY, u32 groupZ) = 0;
         virtual void TransitionImage(const TextureHandle& handle, ETextureLayout newLayout) = 0;
         virtual void AddMemoryBarrier(EAccessFlags srcAccess, EAccessFlags dstAccess, EPipelineStageFlags srcPipeline, EPipelineStageFlags dstPipeline) = 0;
+        virtual void AddMemoryBarrier(const BufferHandle& buffer, EPipelineStageFlags srcPipeline, EPipelineStageFlags dstPipeline) = 0;
         virtual void BeginRendering(const GraphicsPipelineHandle& handle, bool useDepth = false, bool clearAttachments = false, bool writeDepth = false) = 0;
         virtual void BindPipeline(const GraphicsPipelineHandle& handle) = 0;
         virtual void EndRendering() = 0;
         virtual void Draw(u32 vertexCount, u32 instanceCount, u32 firstVertex, u32 firstInstance) = 0;
         virtual void DrawIndexed(u32 indexCount, u32 instanceCount, u32 firstIndex, i32 vertexOffset, u32 firstInstance) = 0;
         virtual void DrawIndexedIndirect(const BufferHandle& indirectBuffer, u64 offset, u32 drawCount) = 0;
-        virtual void BindVertexBuffer(const BufferHandle& vertexBuffer, glm::mat4 transform = glm::mat4(1.f), PBRMaterialData materialData = {}) = 0;
+        virtual void BindVertexBuffer(const BufferHandle& vertexBuffer, glm::mat4 transform = glm::mat4(1.f), u32 materialIndex = U32_MAX) = 0;
         virtual void BindIndexBuffer(const BufferHandle& indexBuffer) = 0;
         
         ECommandBufferState GetState() const { return m_State->load(); }
