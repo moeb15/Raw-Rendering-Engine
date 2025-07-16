b4da24a1e700adf4f4be911bc9c5881074077337
Mujtaba Bader <mujtaba.bader@hotmail.com>
2025-07-16 17:01:00 -0400

    misc

--- raw_renderer/src/editor/editor.cpp
+++ raw_renderer/src/editor/editor.cpp
@@ -20,13 +20,12 @@ namespace Raw
     void Editor::Render(f32 dt, GFX::SceneData& sceneData, GFX::GlobalSceneData& globalData)
     {
         ImGuiIO& io = ImGui::GetIO();
-        ImGui::Begin("Statistics");
+        ImGui::Begin("Editor");
         ImGui::Separator();
+        ImGui::Text("Statistics");
         ImGui::Text("Framerate: %u fps", (u32)io.Framerate);
         ImGui::Text("Frametime: %0.2f ms", dt * 1000.f);
-        ImGui::End();
-
-        ImGui::Begin("Global Scene Data");
+        ImGui::Spacing();
         ImGui::Separator();
         ImGui::Spacing();
         ImGui::Text("Directional Light Settings:");
@@ -34,35 +33,39 @@ namespace Raw
         ImGui::SliderFloat("Y", &globalData.lightDir.y, -1.f, 1.f, "%.3f");
         ImGui::SliderFloat("Z", &globalData.lightDir.z, -1.f, 1.f, "%.3f");
         ImGui::SliderFloat("Intensity", &globalData.lightIntensity, 0.f, 100.f, "%.1f");
-        ImGui::End();
         globalData.lightDir = glm::normalize(globalData.lightDir);
         globalData.lightView = glm::lookAt(glm::vec3(0, 15.f, 0), glm::vec3(-globalData.lightDir.x, -globalData.lightDir.y, -globalData.lightDir.z), glm::vec3(0, 0, 1));
         globalData.lightProj = glm::ortho(-20.f, 20.f, -20.f, 20.f, -15.f, 15.f);
     
-        ImGui::Begin("Meshes");
-        for(u32 i = 0; i < sceneData.meshes.size(); i++)
+        ImGui::Spacing();
+        ImGui::Text("Meshes");
+        if(ImGui::TreeNode("Root"))
         {
-            if(ImGui::TreeNode(&sceneData.meshes[i], "Mesh %u", i))
+            for(u32 i = 0; i < sceneData.meshes.size(); i++)
             {
-                GFX::MeshData& mesh = sceneData.meshes[i];
-                
+                if(ImGui::TreeNode(&sceneData.meshes[i], "Mesh %u", i))
                 {
-                    GFX::PBRMaterialData& materialData = sceneData.materials[mesh.materialIndex];                    
-                    ImGui::Text("Material Data");
-                    ImGui::Spacing();
-                    ImGui::Text("Base Color Factor");
-                    ImGui::SliderFloat("R", &materialData.baseColorFactor.x, 0.0f, 1.0f);
-                    ImGui::SliderFloat("G", &materialData.baseColorFactor.y, 0.0f, 1.0f);
-                    ImGui::SliderFloat("B", &materialData.baseColorFactor.z, 0.0f, 1.0f);
-                    ImGui::SliderFloat("A", &materialData.baseColorFactor.w, 0.0f, 1.0f);
-                    ImGui::Spacing();
-                    ImGui::Text("Metallic Roughness Factor");
-                    ImGui::SliderFloat("Metallic", &materialData.metalRoughnessFactor.x, 0.0f, 1.0f);
-                    ImGui::SliderFloat("Roughness", &materialData.metalRoughnessFactor.y, 0.0f, 1.0f);
+                    GFX::MeshData& mesh = sceneData.meshes[i];
+                    
+                    {
+                        GFX::PBRMaterialData& materialData = sceneData.materials[mesh.materialIndex];                    
+                        ImGui::Text("Material Data");
+                        ImGui::Spacing();
+                        ImGui::Text("Base Color Factor");
+                        ImGui::SliderFloat("R", &materialData.baseColorFactor.x, 0.0f, 1.0f);
+                        ImGui::SliderFloat("G", &materialData.baseColorFactor.y, 0.0f, 1.0f);
+                        ImGui::SliderFloat("B", &materialData.baseColorFactor.z, 0.0f, 1.0f);
+                        ImGui::SliderFloat("A", &materialData.baseColorFactor.w, 0.0f, 1.0f);
+                        ImGui::Spacing();
+                        ImGui::Text("Metallic Roughness Factor");
+                        ImGui::SliderFloat("Metallic", &materialData.metalRoughnessFactor.x, 0.0f, 1.0f);
+                        ImGui::SliderFloat("Roughness", &materialData.metalRoughnessFactor.y, 0.0f, 1.0f);
+                    }
+                    
+                    ImGui::TreePop();
                 }
-                
-                ImGui::TreePop();
             }
+            ImGui::TreePop();
         }
         ImGui::End();
     }
