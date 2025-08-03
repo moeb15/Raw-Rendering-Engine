a32dd8edb941801026d2c5856c3a7d46ba53a658
Mujtaba Bader <mujtaba.bader@hotmail.com>
2025-08-03 07:00:43 -0400

    custom string implementation

---
+++ raw_renderer/include/core/string.hpp
@@ -0,0 +1,67 @@
+#pragma once
+
+#include "core/defines.hpp"
+#include "memory/memory_service.hpp"
+#include <cstring>
+
+namespace Raw::rstd
+{
+    class string
+    {
+    public:
+        string();
+        ~string();
+
+        string(cstring val);
+        
+        RAW_INLINE string(const string& src)
+        {
+            m_Len = src.length();
+            m_Str = (char*)RAW_ALLOCATE(sizeof(char) * (m_Len + 1), alignof(char));
+            strcpy(m_Str, src.c_str());
+            m_Str[m_Len] = '\0';
+        }
+        
+        RAW_INLINE string& operator=(const string& src)
+        {
+            this->clear();
+            this->m_Len = src.length();
+            this->m_Str = (char*)RAW_ALLOCATE(sizeof(char) * (m_Len + 1), alignof(char));
+            strcpy(m_Str, src.c_str());
+            this->m_Str[m_Len] = '\0';
+            return *this;
+        }
+
+        RAW_INLINE string(string&& src)
+        {
+            m_Str = src.m_Str;
+            m_Len = src.m_Len;
+            src.clear();
+        }
+        
+        RAW_INLINE string& operator=(string&& src)
+        {
+            m_Str = src.m_Str;
+            m_Len = src.m_Len;
+            src.clear();
+            return *this;
+        }
+
+        bool operator==(const string& val)
+        {
+            if(this->m_Len != val.m_Len) return false;
+            return !strcmp(this->c_str(), val.c_str());
+        }
+
+        RAW_INLINE cstring c_str() const { return m_Str; }
+        RAW_INLINE u32 length() const { return m_Len; }
+    
+    private:
+        void clear();
+
+    private:
+        char* m_Str{ nullptr };
+        u32 m_Len{ 0 };
+
+    };
+}
---
+++ raw_renderer/src/core/string.cpp
@@ -0,0 +1,42 @@
+#include "core/string.hpp"
+#include "core/logger.hpp"
+
+namespace Raw::rstd
+{
+    string::string()
+    {
+        m_Str = (char*)RAW_ALLOCATE(sizeof(char), alignof(char));
+        m_Str[0] = '\0';
+        m_Len = 0;
+    }
+
+    void string::clear()
+    {
+        if(m_Str) RAW_DEBUG("Deleting string '%s' at address %p", m_Str, m_Str);
+        RAW_DEALLOCATE(m_Str);
+        m_Len = 0;
+        m_Str = nullptr;
+    }
+
+    string::~string()
+    {
+        clear();
+    }
+    
+    string::string(cstring val)
+    {
+        if(val == nullptr)
+        {
+            m_Str = (char*)RAW_ALLOCATE(sizeof(char), alignof(char));
+            m_Str[0] = '\0';
+            m_Len = 0;
+        }
+        else
+        {
+            m_Len = (u32)strlen(val);
+            m_Str = (char*)RAW_ALLOCATE(sizeof(char) * (m_Len + 1), alignof(char));
+            strcpy(m_Str, val);
+            m_Str[m_Len] = '\0';
+        }
+    }
+}
